4bf8fb0dc8334ca0a33bc3e0af3946b2fbb763cc
John <lawchihon@ymail.com>
2017-02-27 00:35:05 +0000

    Initial commit

---
+++ public/staff/salespeople/delete.php
@@ -0,0 +1,47 @@
+<?php
+require_once('../../../private/initialize.php');
+
+if(!isset($_GET['id'])) {
+  redirect_to('index.php');
+}
+$salespeople_result = find_salesperson_by_id($_GET['id']);
+// No loop, only one result
+$salesperson = db_fetch_assoc($salespeople_result);
+
+// Set default values for all variables the page needs.
+$errors = array();
+
+if(is_post_request()) {
+  if ($_POST['submit'] == "No") {
+    redirect_to('show.php?id=' . u($salesperson['id']));
+  }
+
+  $result = delete_salesperson($salesperson);
+  if($result === true) {
+    redirect_to('./index.php');
+  } else {
+    $errors = $result;
+  }
+}
+?>
+<?php $page_title = 'Staff: Delete Salesperson ' . h($salesperson['first_name']) . " " . h($salesperson['last_name']); ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="main-content">
+  <a href="index.php" class="btn btn-primary">Back to User Detail</a><br />
+
+  <h1><small>Delete Salesperson:</small> <?php echo h($salesperson['first_name']) . " " . h($salesperson['last_name']); ?></h1>
+
+  <?php echo display_errors($errors); ?>
+
+  <form action="#" method="post" style="text-align: center;">
+    <label>Are you sure you want to permanently delete the salesperson:
+    <?php echo h($salesperson['first_name']) . " " . h($salesperson['last_name']); ?></label>
+    <br />
+    <input type="submit" name="submit" value="Yes" class="btn btn-success" />
+    <input type="submit" name="submit" value="No" class="btn btn-danger" />
+  </form>
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
---
+++ sql/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `created_at` DATETIME DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10;
